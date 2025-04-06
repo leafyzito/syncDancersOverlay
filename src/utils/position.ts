e52e0982d7158450499ca837424fbc3deadb811c
leafyzito <rafaelzao2003@gmail.com>
2025-04-06 20:05:58 +0100

    fix avatars going off screen

--- src/utils/position.ts
+++ src/utils/position.ts
@@ -3,11 +3,12 @@ import { configService } from '../services/config';
 export const getRandomPosition = () => {
     const config = configService.getConfig();
     const scaledAvatarSize = config.display.avatarSize * config.ui.scale;
-    const barrierSize = 50 * config.ui.scale; // 10px barrier scaled by UI scale
+    const barrierSize = 100 * config.ui.scale; // 50px barrier scaled by UI scale
     const maxX = window.innerWidth - scaledAvatarSize - barrierSize;
+    const minX = barrierSize;
 
     return {
-        x: Math.random() * maxX + barrierSize,
+        x: Math.random() * (maxX - minX) + minX,
         y: Math.random() * (config.animation.movementRange.y.max - config.animation.movementRange.y.min) + config.animation.movementRange.y.min
     };
 }; 
